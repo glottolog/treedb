550b2030ffefe382ccb65444f9f904d5d1d0b709
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-07-02 18:12:15 +0200

    move example session

--- README.rst
+++ README.rst
@@ -6,3 +6,48 @@ Links
 -----
 
 - GitHub: https://github.com/glottolog/treedb
+
+
+Example session (glottolog cloned into the same directory as this repo)
+
+```sh
+$ python -m venv .venv  # PY3
+$ source .venv/bin/activate  # Windows: $ .venv/Scripts/activate.bat
+$ pip install -e .
+
+$ python
+```
+
+```python
+>>> import treedb
+>>> next(treedb.iterlanguoids())
+{'id': 'abin1243', 'parent_id': None, 'level': 'language', ...
+
+>>> treedb.load()
+...
+'treedb.sqlite3'
+
+>>> treedb.check()
+...
+
+>>> treedb.export_db()
+'treedb.zip'
+
+>>> treedb.write_csv()
+'treedb.csv'
+
+>>> treedb.load(rebuild=True)
+...
+'treedb.sqlite3'
+
+>>> import sqlalchemy as sa
+>>> treedb.write_csv(sa.select([treedb.Languoid]), filename='languoids.csv')
+
+>>> sa.select([treedb.Languoid], bind=treedb.engine).execute().first()
+('abin1243', 'language', 'Abinomn', None, 'bsa', 'bsa', -2.92281, 138.891)
+
+>>> session = treedb.Session()
+>>> session.query(treedb.Languoid).first()
+<Languoid id='abin1243' level='language' name='Abinomn' hid='bsa' iso639_3='bsa'>
+>>> session.close()
+```
--- treedb/__init__.py
+++ treedb/__init__.py
@@ -1,44 +1,6 @@
 # treedb - load ../../glottolog/languoids/tree/**/md.ini into sqlite3
 
-"""Example session (glottolog cloned into the same directory as this repo)
-
-$ python -m venv .venv  # PY3
-$ source .venv/bin/activate  # Windows: $ .venv/Scripts/activate.bat
-$ pip install -e .
-
-$ python
->>> import treedb
->>> next(treedb.iterlanguoids())
-{'id': 'abin1243', 'parent_id': None, 'level': 'language', ...
-
->>> treedb.load()
-...
-'treedb.sqlite3'
-
->>> treedb.check()
-...
-
->>> treedb.export_db()
-'treedb.zip'
-
->>> treedb.write_csv()
-'treedb.csv'
-
->>> treedb.load(rebuild=True)
-...
-'treedb.sqlite3'
-
->>> import sqlalchemy as sa
->>> treedb.write_csv(sa.select([treedb.Languoid]), filename='languoids.csv')
-
->>> sa.select([treedb.Languoid], bind=treedb.engine).execute().first()
-('abin1243', 'language', 'Abinomn', None, 'bsa', 'bsa', -2.92281, 138.891)
-
->>> session = treedb.Session()
->>> session.query(treedb.Languoid).first()
-<Languoid id='abin1243' level='language' name='Abinomn' hid='bsa' iso639_3='bsa'>
->>> session.close()
-"""
+"""Load glottolog lanuoid tree ini files into SQLite3 database."""
 
 from . _compat import pathlib
 
