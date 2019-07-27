b1f004b905399693783ffee5b5e24204fc6fd52e
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-07-28 01:35:22 +0200

    refactor iterlanguoids to use the mapping protocol

--- treedb/languoids.py
+++ treedb/languoids.py
@@ -4,27 +4,32 @@ from __future__ import unicode_literals
 
 import re
 import datetime
+import functools
 
 from . import files as _files
 
 __all__ = ['iterlanguoids']
 
 
-def getlines(cfg, section, option):
-    if not cfg.has_option(section, option):
-        return []
-    return cfg.get(section, option).strip().splitlines()
-
-
-def getdate(cfg, section, option, format_='%Y-%m-%d', **kwargs):
-    value = cfg.get(section, option, **kwargs)
-    if value is None:
+def get_type(mapping, key, type_):
+    result = mapping.get(key)
+    if result is None:
         return None
+    return type_(result)
+
+
+def make_lines(value):
+    if value is None:
+        return []
+    return value.strip().splitlines()
+
+
+def make_date(value, format_='%Y-%m-%d'):
     return datetime.datetime.strptime(value, format_).date()
 
-
-def getdatetime(cfg, section, option, format_='%Y-%m-%dT%H:%M:%S'):
-    return datetime.datetime.strptime(cfg.get(section, option), format_)
+    
+def make_datetime(value, format_='%Y-%m-%dT%H:%M:%S'):
+    return datetime.datetime.strptime(value, format_)
 
 
 def splitcountry(name, _match=re.compile(r'(.+) \(([^)]+)\)$').match):
@@ -50,7 +55,7 @@ def splitlink(markdown, _match=re.compile(
 def splitsource(s, _match=re.compile(
     r"\*\*(?P<bibfile>[a-z0-9\-_]+):(?P<bibkey>[a-zA-Z.?\-;*'/()\[\]!_:0-9\u2014]+?)\*\*"
     r"(:(?P<pages>[0-9\-f]+))?"
-    r'(<trigger "(?P<trigger>[^\"]+)">)?').match):
+    r'(?:<trigger "(?P<trigger>[^\"]+)">)?').match):
     return _match(s).groupdict()
 
 
@@ -66,61 +71,65 @@ def splitaltname(s, _match=re.compile(
 def iterlanguoids(root=None):
     """Yield dicts from ../../languoids/tree/**/md.ini files."""
     for path_tuple, _, cfg in _files.iterconfig(root):
+        core = cfg['core']
         item = {
             'id': path_tuple[-1],
             'parent_id': path_tuple[-2] if len(path_tuple) > 1 else None,
-            'level': cfg.get('core', 'level'),
-            'name': cfg.get('core', 'name'),
-            'hid': cfg.get('core', 'hid', fallback=None),
-            'iso639_3': cfg.get('core', 'iso639-3', fallback=None),
-            'latitude': cfg.getfloat('core', 'latitude', fallback=None),
-            'longitude': cfg.getfloat('core', 'longitude', fallback=None),
-            'macroareas': getlines(cfg, 'core', 'macroareas'),
-            'countries': [splitcountry(c) for c in getlines(cfg, 'core', 'countries')],
-            'links': [splitlink(c) for c in getlines(cfg, 'core', 'links')],
+            'level': core['level'],
+            'name': core['name'],
+            'hid': core.get('hid'),
+            'iso639_3': core.get('iso639-3'),
+            'latitude': get_type(core, 'latitude', float),
+            'longitude': get_type(core, 'longitude', float),
+            'macroareas': make_lines(core.get('macroareas')),
+            'countries': [splitcountry(c) for c in make_lines(core.get('countries'))],
+            'links': [splitlink(c) for c in make_lines(core.get('links'))],
         }
-        if cfg.has_section('sources'):
-            item['sources'] = {provider: [splitsource(p) for p in getlines(cfg, 'sources', provider)]
-                               for provider in cfg.options('sources')}
-        if cfg.has_section('altnames'):
-            item['altnames'] = {provider: [splitaltname(a) for a in getlines(cfg, 'altnames', provider)]
-                                for provider in cfg.options('altnames')}
-        if cfg.has_section('triggers'):
-            item['triggers'] = {field: getlines(cfg, 'triggers', field)
-                                for field in cfg.options('triggers')}
-        if cfg.has_section('identifier'):
+        if 'sources' in cfg:
+            item['sources'] = {provider: [splitsource(p) for p in make_lines(sources)]
+                               for provider, sources in cfg['sources'].items()}
+        if 'altnames' in cfg:
+            item['altnames'] = {provider: [splitaltname(a) for a in make_lines(altnames)]
+                                for provider, altnames in cfg['altnames'].items()}
+        if 'triggers' in cfg:
+            item['triggers'] = {field: make_lines(triggers)
+                                for field, triggers in cfg['triggers'].items()}
+        if 'identifier' in cfg:
             # FIXME: semicolon-separated (wals)?
-            item['identifier'] = dict(cfg.items('identifier'))
-        if cfg.has_section('classification'):
+            item['identifier'] = dict(cfg['identifier'])
+        if 'classification' in cfg:
             item['classification'] = {
-                c: list(map(splitsource, getlines(cfg, 'classification', c)))
+                c: list(map(splitsource, make_lines(classifications)))
                    if c.endswith('refs') else
-                   cfg.get('classification', c)
-                for c in cfg.options('classification')}
+                   classifications
+                for c, classifications in cfg['classification'].items()}
             assert item['classification']
-        if cfg.has_section('endangerment'):
+        if 'endangerment' in cfg:
+            sct = cfg['endangerment']
             item['endangerment'] = {
-                'status': cfg.get('endangerment', 'status'),
-                'source': cfg.get('endangerment', 'source'),
-                'date': getdatetime(cfg, 'endangerment', 'date'),
-                'comment': cfg.get('endangerment', 'comment'),
+                'status': sct['status'],
+                'source': sct['source'],
+                'date': make_datetime(sct['date']),
+                'comment': sct['comment'],
             }
-        if cfg.has_section('hh_ethnologue_comment'):
+        if 'hh_ethnologue_comment' in cfg:
+            sct = cfg['hh_ethnologue_comment']
             item['hh_ethnologue_comment'] = {
-                'isohid': cfg.get('hh_ethnologue_comment', 'isohid'),
-                'comment_type': cfg.get('hh_ethnologue_comment', 'comment_type'),
-                'ethnologue_versions': cfg.get('hh_ethnologue_comment', 'ethnologue_versions'),
-                'comment': cfg.get('hh_ethnologue_comment', 'comment'),
+                'isohid': sct['isohid'],
+                'comment_type': sct['comment_type'],
+                'ethnologue_versions': sct['ethnologue_versions'],
+                'comment': sct['comment'],
             }
-        if cfg.has_section('iso_retirement'):
+        if 'iso_retirement' in cfg:
+            sct = cfg['iso_retirement']
             item['iso_retirement'] = {
-                'code': cfg.get('iso_retirement', 'code'),
-                'name': cfg.get('iso_retirement', 'name'),
-                'change_request': cfg.get('iso_retirement', 'change_request', fallback=None),
-                'effective': getdate(cfg, 'iso_retirement', 'effective'),
-                'reason': cfg.get('iso_retirement', 'reason'),
-                'change_to': getlines(cfg, 'iso_retirement', 'change_to'),
-                'remedy': cfg.get('iso_retirement', 'remedy', fallback=None),
-                'comment': cfg.get('iso_retirement', 'comment', fallback=None),
+                'code': sct['code'],
+                'name': sct['name'],
+                'change_request': sct.get('change_request'),
+                'effective': make_date(sct['effective']),
+                'reason': sct['reason'],
+                'change_to': make_lines(sct.get('change_to')),
+                'remedy': sct.get('remedy'),
+                'comment': sct.get('comment'),
             }
         yield item
