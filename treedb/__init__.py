6d00a956cab987618c19dffda5e55b3f3273f221
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-03-25 23:25:21 +0100

    bump version for development

--- treedb/__init__.py
+++ treedb/__init__.py
@@ -39,7 +39,7 @@ __all__ = ['ROOT', 'ENGINE',
            'root', 'engine']
 
 __title__ = 'treedb'
-__version__ = '0.3.1'
+__version__ = '0.3.2.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2017-2020 Sebastian Bank'
