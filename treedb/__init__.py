40096af545af63af500bf2992e3c4ffcb97c6c99
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-03-29 23:32:47 +0200

    release 0.5

--- treedb/__init__.py
+++ treedb/__init__.py
@@ -45,7 +45,7 @@ __all__ = ['ROOT', 'ENGINE',
            'root', 'engine']
 
 __title__ = 'treedb'
-__version__ = '0.5.dev0'
+__version__ = '0.5'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2017-2020 Sebastian Bank'
