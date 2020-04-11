0efb97037e221aef625bafadfc75be1a2661a838
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-04-11 14:22:10 +0200

    add test for print_query_sql()

---
+++ tests/test_backend.py
@@ -0,0 +1,31 @@
+import pytest
+
+import treedb
+
+
+@pytest.mark.parametrize('query, pretty, expected', [
+    (treedb.select([treedb.Languoid]), False, ('SELECT languoid.id,'
+                                               ' languoid.name,'
+                                               ' languoid.level,'
+                                               ' languoid.parent_id,'
+                                               ' languoid.hid,'
+                                               ' languoid.iso639_3,'
+                                               ' languoid.latitude,'
+                                               ' languoid.longitude \n'
+                                               'FROM languoid\n')),
+    (treedb.select([treedb.Languoid]), True, ('SELECT languoid.id,\n'
+                                              '       languoid.name,\n'
+                                              '       languoid.level,\n'
+                                              '       languoid.parent_id,\n'
+                                              '       languoid.hid,\n'
+                                              '       languoid.iso639_3,\n'
+                                              '       languoid.latitude,\n'
+                                              '       languoid.longitude\n'
+                                              'FROM languoid\n')),
+])
+def test_print_query_sql(capsys, query, pretty, expected):
+    assert treedb.print_query_sql(query, pretty=pretty) is None
+
+    out, err = capsys.readouterr()
+    assert not err
+    assert out == expected
