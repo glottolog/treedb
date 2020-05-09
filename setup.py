7bc76938c232b4d07e74c1927786bca9da8dd625
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-05-09 16:48:38 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treedb',
-    version='1.3.1',
+    version='1.3.2.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Glottolog languoid tree as SQLite database',
