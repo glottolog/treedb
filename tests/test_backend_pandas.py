481cddfe0a92b14fc23b87b77d383d518006e596
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-03-26 13:49:48 +0100

    fix test_pd_read_json_lines()

--- tests/test_backend_pandas.py
+++ tests/test_backend_pandas.py
@@ -22,7 +22,7 @@ def test_pd_read_json_lines(treedb):
     # TODO: check memory usage
     df = treedb.backend.pandas.pd_read_json_lines()
 
-    if treedb.shortcuts.PANDAS is None:
+    if treedb.backend.pandas.PANDAS is None:
         assert df is None
     else:
         assert not df.empty
