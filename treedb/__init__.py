24cf7b18e5f702f2fab61ed8834216d3a2389672
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-07-11 09:35:26 +0200

    reformat imports

--- treedb/__init__.py
+++ treedb/__init__.py
@@ -1,15 +1,12 @@
 """Load Glottolog lanuoid tree ``md.ini`` files into SQLite3 database."""
 
 from ._globals import SESSION as Session
-
 from ._tools import sha256sum
-
 from .backend import (print_versions,
                       set_engine,
                       connect,
                       scalar,
                       iterrows)
-
 from .backend.export import (print_dataset,
                              print_schema,
                              print_query_sql,
@@ -19,47 +16,26 @@ from .backend.export import (print_dataset,
                              print_rows,
                              write_csv,
                              hash_csv)
-
 from .backend.load import main as load
-
-from .backend.models import (Dataset,
-                             Producer,
-                             Config)
-
-from .backend.pandas import (pd_read_sql,
-                             pd_read_json_lines)
-
-from .backend.sqlite_master import (print_table_sql,
-                                    select_tables_nrows)
-
+from .backend.models import Dataset, Producer, Config
+from .backend.pandas import pd_read_sql, pd_read_json_lines
+from .backend.sqlite_master import print_table_sql, select_tables_nrows
 from .backend.views import TABLES as views
-
 from .languoids import set_root, iterfiles
-
-from .checks import (check,
-                     compare_languoids)
-
+from .checks import check, compare_languoids
 from .export import (print_languoid_stats,
                      iterlanguoids,
                      checksum,
                      write_json_lines as write_languoids,
                      pd_read_languoids,
                      write_files)
-
 from .glottolog import checkout_or_clone
-
 from .logging_ import configure_logging
-
-from .models import (LEVEL,
-                     Languoid)
-
+from .models import LEVEL, Languoid
 from .queries import (get_example_query,
                       get_json_query as get_languoids_query,
                       iterdescendants)
-
-from .settings import (configure,
-                       get_default_root)
-
+from .settings import configure, get_default_root
 
 __all__ = ['Session',
            'sha256sum',
