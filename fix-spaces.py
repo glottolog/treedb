cd9a6f6a820b1f2c42be7c178fbd44024daa07dc
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-11-17 22:44:56 +0100

    drop double spaces in sub and one more in family

--- fix-spaces.py
+++ fix-spaces.py
@@ -19,7 +19,7 @@ update = (sa.update(treedb.raw.Value)
           .where(sa.select(1)
                  .where(treedb.raw.Value.option_id == treedb.raw.Option.id)
                  .where(treedb.raw.Option.section == 'classification')
-                 .where(treedb.raw.Option.option == 'family')
+                 .where(treedb.raw.Option.option.in_(['sub', 'family']))
                  .exists())
           .where(treedb.raw.Value.value.like('%  %'))
           .values(value=sa.func.replace(treedb.raw.Value.value, '  ', ' ')))
@@ -30,6 +30,6 @@ with treedb.connect() as conn:
     result = conn.execute(update)
     conn.commit()
 
-print('', f'{result.rowcount:d} lines replaced', sep='\n')  # 41, 0
+print('', f'{result.rowcount:d} lines replaced', sep='\n')  # 42, 0
 
-#print('', f"{treedb.write_files(source='raw')} files written", sep='\n')  # 41, 0
+#print('', f"{treedb.write_files(source='raw')} files written", sep='\n')  # 42, 0
