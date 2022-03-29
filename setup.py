c2bd7c636290c92f0dff2e9bd955030f5dc82bb7
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2022-03-29 23:45:22 +0200

    bump pinned pycountry to 22.3.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     python_requires='>=3.7',
     install_requires=[
         'csv23~=0.3',
-        'pycountry==20.7.3',  # date-based versioning scheme
+        'pycountry==22.3.5',  # date-based versioning scheme
         'sqlalchemy~=1.4',
     ],
     extras_require={
