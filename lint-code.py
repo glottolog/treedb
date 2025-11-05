359913ac7ba3fe07ef591ef6992bc27db511735a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2025-11-05 15:13:18 +0100

    run linter via flake8.main.cli.main()

--- lint-code.py
+++ lint-code.py
@@ -1,31 +1,20 @@
 #!/usr/bin/env python3
-# flake8: noqa
 
 """Run code linting with https://flake8.pycqa.org."""
 
 import pathlib
-import platform
-import subprocess
 import sys
 
-SELF = pathlib.Path(__file__)
-
-PYTHON = 'py' if platform.system() == 'Windows' else 'python'
-
-CMD = [PYTHON, '-m', 'flake8']
+import flake8.main.cli
 
 
-print('run', [SELF.name] + sys.argv[1:])
-cmd = CMD + sys.argv[1:]
+if __name__ == '__main__':
+    print('run', [pathlib.Path(__file__).name] + sys.argv[1:])
+    args = sys.argv[1:]
 
-print(f'subprocess.run({cmd!r})')
-try:
-    proc = subprocess.run(cmd, check=True)
-except subprocess.CalledProcessError as e:
-    assert e.returncode != 0, f'non-zero returncode: {e}'
-    print('FAILED:', e)
-    sys.exit(e.returncode)
-else:
-    assert proc.returncode == 0, f'passed: {proc}'
-    print('PASSED:', proc)
-    sys.exit(proc.returncode)
+    # https://flake8.pycqa.org/en/latest/internal/cli.html#flake8.main.cli.main
+    print(f'flake8.main.cli.main({args!r})')
+    if (returncode := flake8.main.cli.main(args)):
+        print('FAILED:', returncode)
+        sys.exit(returncode)
+    print('PASSED.')
